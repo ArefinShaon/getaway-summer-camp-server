2e3733df691662d6ac53320f08b914088e337815
ArefinShaon <arefinshaon99@gmail.com>
2023-06-09 01:10:34 +0600

    delete done

--- index.js
+++ index.js
@@ -90,12 +90,12 @@ async function run() {
 
 
         // DElete
-        // app.delete("/users/:id", async (req, res) => {
-        //   const id = req.params.id;
-        //   const query = { _id: new ObjectId(id) };
-        //   const result = await userCollection.deleteOne(query);
-        //   res.send(result);
-        // });
+        app.delete("/users/:id", async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await userCollection.deleteOne(query);
+          res.send(result);
+        });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
