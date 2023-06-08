fdf62ddb17161a59ddc6b8e2c5c8da6b6582c1c3
ArefinShaon <arefinshaon99@gmail.com>
2023-06-09 02:55:26 +0600

    bug fixes

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function run() {
       }
     });
 
-
+// get data with specific id
     app.get("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
